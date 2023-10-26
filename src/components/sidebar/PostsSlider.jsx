e5a29717611e280763a17215c572c3435d20df10
Carl0sPineda <carlos7712p@gmail.com>
2023-10-26 09:22:42 -0600

    input searchByTitle with styles

--- src/components/sidebar/PostsSlider.jsx
+++ src/components/sidebar/PostsSlider.jsx
@@ -1,8 +1,7 @@
 import { useValue } from "../../context/ContextProvider";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Box, InputAdornment, TextField } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
+import search from "../../assets/search.svg";
 
 const PostsSlider = () => {
   const navigate = useNavigate();
@@ -11,11 +10,16 @@ const PostsSlider = () => {
   const [filteredPosts, setFilteredPosts] = useState(state.posts);
 
   useEffect(() => {
+    // Función para eliminar tildes y caracteres acentuados
+    function removeAccents(text) {
+      return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    }
+
     // Función para filtrar los posts por título
     const filterPostsByTitle = () => {
-      const lowercaseText = titleFilter.toLowerCase();
+      const lowercaseText = removeAccents(titleFilter.toLowerCase()); // Normaliza el texto
       const filtered = state.posts.filter((post) =>
-        post.title.toLowerCase().includes(lowercaseText)
+        removeAccents(post.title.toLowerCase()).includes(lowercaseText)
       );
       setFilteredPosts(filtered);
     };
@@ -30,39 +34,64 @@ const PostsSlider = () => {
 
   return (
     <div className="root">
-      <input
-        type="text"
-        style={{
-          width: "70%",
-          height: "40px",
-          display: "flex",
-          margin: "auto",
-          background: "white",
-          color: "white",
-          fontSize: "14px",
-          borderRadius: "5px",
-        }}
-        placeholder="Buscar una ubicación"
-        value={titleFilter}
-        onChange={handleTitleFilterChange}
-      />
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <div style={{ position: "relative" }}>
+          <img
+            src={search} // Reemplaza con la ruta correcta a tu SVG
+            alt="Search Icon"
+            style={{
+              width: "24px", // Ajusta el ancho y alto del icono según tus necesidades
+              height: "24px",
+              position: "absolute",
+              left: "30px", // Ajusta la posición del icono al inicio izquierdo
+              top: "50%",
+              transform: "translateY(-50%)",
+            }}
+          />
+        </div>
+        <input
+          type="text"
+          className="search"
+          style={{
+            width: "70%",
+            padding: "10px",
+            display: "flex",
+            margin: "auto",
+            background: "transparent",
+            color: "#333",
+            fontSize: "16px",
+            borderRadius: "25px",
+            border: "2px solid #bfcbde",
+            paddingLeft: "45px", // Aumenta el padding izquierdo para acomodar el ícono
+            paddingRight: "15px",
+            outline: "none",
+          }}
+          placeholder="Buscar una ubicación"
+          value={titleFilter}
+          onChange={handleTitleFilterChange}
+        />
+      </div>
       <div className="container">
         <div className="card-container">
-          {filteredPosts.map((post) => (
-            <div key={post._id} className="card">
-              <div className="card-image">
-                <img
-                  src={post.images[0]}
-                  alt={post.title}
-                  loading="lazy"
-                  onClick={() => navigate(`/post/${post._id}`)}
-                />
+          {filteredPosts.length === 0 ? (
+            <p>No se encontraron resultados</p>
+          ) : (
+            filteredPosts.map((post) => (
+              <div key={post._id} className="card">
+                <div className="card-image">
+                  <img
+                    src={post.images[0]}
+                    alt={post.title}
+                    loading="lazy"
+                    onClick={() => navigate(`/post/${post._id}`)}
+                  />
+                </div>
+                <div className="card-content">
+                  <h2 className="title">{post.title}</h2>
+                </div>
               </div>
-              <div className="card-content">
-                <h2 className="title">{post.title}</h2>
-              </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
       </div>
     </div>
